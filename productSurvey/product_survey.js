1437d93d7606bb41353086c072236774395924ec
Christopher Dellosa <christopher.j.dellosa@gmail.com>
2025-11-18 14:01:42 -0800

    product survey

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,33 +1,35 @@
 /* Step 2 */
 
 function submitFeedback() {
-    const username = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-    const email = document.getElementById('email').value;
-    const job = document.getElementById('job').value;
-    const designation = document.getElementById('designation').value;
-    const productType = document.getElementById('productType').value;
-    const feedback = document.getElementById('feedbackText').value;
+  const username = document.getElementById('name').value;
+  const age = document.getElementById('age').value;
+  const email = document.getElementById('email').value;
+  const job = document.getElementById('job').value;
+  const designation = document.getElementById('designation').value;
+  const productType = document.getElementById('productType').value;
+  const feedback = document.getElementById('feedbackText').value;
 
-    const submitButton=document.getElementById('submitBtn');
+  document.getElementById('userName').innerHTML = username;
+  document.getElementById('userAge').innerHTML = age;
+  document.getElementById('userEmail').innerHTML = email;
+  document.getElementById('userJob').innerHTML = job;
+  document.getElementById('userDesignation').innerHTML = designation;
+  document.getElementById('userProductChoice').innerHTML = productType;
+  document.getElementById('userFeedback').innerHTML = feedback;
 
-    submitButton.onclick = submitFeedback;
+  alert('Thank you for your valuable feedback');
 
-    alert('Thank you for your valuable feedback')
-
-    document.getElementById('userName').innerHTML = username;
-    document.getElementById('userAge').innerHTML = age;
-    document.getElementById('userEmail').innerHTML = email;
-    document.getElementById('userJob').innerHTML = job;
-    document.getElementById('userDesignation').innerHTML = designation;
-    document.getElementById('userProductChoice').innerHTML = productType;
-    document.getElementById('userFeedback').innerHTML = feedback;
-
-    document.getElementById('userInfo').style.display = 'block';
+  const data = {username, age, email, job, designation, productType, feedback};
+  console.log('data', data);
 }
 
+const submitButton = document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
+
+document.getElementById('userInfo').style.display = 'block';
+
 document.addEventListener('keydown', function(event) {
-   if (event.key === 'Enter') {
-     submitFeedback();
-   }
- });
+  if (event.key === 'Enter') {
+    submitFeedback();
+  }
+});
